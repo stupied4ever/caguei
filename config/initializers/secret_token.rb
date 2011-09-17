be3f613f9d25d494d71cffa317e85b576e58f233
Rafael Silva Almeida <eusou@Rafael-Silva-Almeidas-Mac-mini.local>
2011-09-16 21:00:33 -0300

    comencando o projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caguei::Application.config.secret_token = '6945b269581cb114348c918c326b4d9048e19cf12c383b033436a97f119df1cb13309cdc0357dc2636ba5677ef4dd59e4083ac8f430b9c6477601334e954eb10'
