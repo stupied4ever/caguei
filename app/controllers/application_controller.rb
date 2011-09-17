10368c4d9dff0037f6f68ea38be6718c13018cf4
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2011-09-16 23:05:17 -0300

    brincando com o facebooker2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   include Facebooker2::Rails::Controller
   protect_from_forgery
+   before_filter :set_fb_session
 
+  def set_fb_session
+    session[:fb_user] = current_facebook_user.fetch if session[:fb_user].nil? and current_facebook_user
+  end
 end
